eb672ccb74d9400541ba3ae597115e5de4e2738c
Assa-goDori <62422946+Assa-goDori@users.noreply.github.com>
2020-08-03 09:50:11 +0900

    Update MemberController.java

--- savedogs/src/main/java/controller/MemberController.java
+++ savedogs/src/main/java/controller/MemberController.java
@@ -1,7 +1,9 @@
 package controller;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import logic.DogService;
 import logic.Member;
 
 @Controller
 @RequestMapping("member")
 public class MemberController {
+	@Autowired
+	private DogService service;
 	
 	@GetMapping("*")
 	public ModelAndView memberEntry() {
@@ -30,6 +35,34 @@ public class MemberController {
 			bresult.reject("error.input.user");
 			mav.getModel().putAll(bresult.getModel());
 			return mav;
+		}
+		try {
+			service.memberInsert(mem);
+			mav.setViewName("redirect:login.dog");
+		} catch(Exception e) {
+			e.printStackTrace();
+			mav.getModel().putAll(bresult.getModel());
+		}
+		return mav;
+	}
+	
+	@PostMapping("login")
+	public ModelAndView login(@Valid Member mem, BindingResult bresult, HttpSession session) {
+		ModelAndView mav = new ModelAndView();
+		/*
+		 * if(bresult.hasErrors()) { bresult.reject("error.input.user"); return mav; }
+		 */
+		try {
+		   Member dbmem = service.getMember(mem.getMember_id());
+		   if(mem.getMember_pass().equals(dbmem.getMember_pass())) {
+ 			  session.setAttribute("login", dbmem);
+			  mav.setViewName("redirect:../main.dog");
+		   } else {
+			  bresult.reject("error.login.password");
+		   }
+		} catch (Exception e) {
+			e.printStackTrace();
+			bresult.reject("error.login.id");
 		}
 		return mav;
 	}
