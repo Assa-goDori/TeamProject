f436fee212b7f5a089eeebfc04457fe79715fd03
Hyo-Young-Sim <68977865+Hyo-Young-Sim@users.noreply.github.com>
2020-08-12 15:39:26 +0900

    1

--- savedogs/src/main/java/logic/AdoptUtils.java
+++ savedogs/src/main/java/logic/AdoptUtils.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AdoptUtils {
-	public static Map<String, String> getState() {
+	public static Map<String, String> getKind() {
 
 		Map<String, String> result = new HashMap<String, String>();
 		
-//		result.put();
+		result.put("000054", "골든 리트리버");
 		
 		return result;
 	}
