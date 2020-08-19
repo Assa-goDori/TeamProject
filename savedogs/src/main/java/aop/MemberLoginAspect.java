a3578f6b6dd31d2e63ea3f8286f25536100c0508
DongUk <67453978+uddone@users.noreply.github.com>
2020-08-19 11:36:44 +0900

    aop
    aop

--- savedogs/src/main/java/aop/MemberLoginAspect.java
+++ savedogs/src/main/java/aop/MemberLoginAspect.java
@@ -15,20 +15,34 @@ import logic.Member;;
 @Aspect		
 @Order(1)	
 public class MemberLoginAspect {
-	@Around // adivce중 하나.
-	("execution(* controller.Item*.chkm*(..)) && args(..,session)")
-	public Object I_M_LoginCheck(ProceedingJoinPoint joinPoint,HttpSession session) throws Throwable{
-		Member loginmem = (Member)session.getAttribute("loginmem");
+	@Around // 일반회원의 로그인 여부를 판단합니다.  	
+//			("execution(* controller. <Item>*.chkm*(..)) && args(..,session)")  : <Item> 부분을 아래 Cart와 같이 바꾸어서 사용하시길 바랍니다.
+//			매개변수의 위치는 상관이 없으나 HttpSession를 꼭 포함해야 합니다.
+	("execution(* controller.Item*.chkm*(..))")
+	public Object ItemMemberLoginCheck(ProceedingJoinPoint joinPoint) throws Throwable{
+		Member loginmem = null;
+		for(Object o : joinPoint.getArgs()) {
+			if(o instanceof HttpSession) {
+				HttpSession session = (HttpSession)o;
+				loginmem = (Member)session.getAttribute("loginmem");
+			}
+		}
 		if(loginmem == null) {
 			throw new LoginException("로그인 후 거래하세요","../member/login.dog");
 		}
 		Object ret = joinPoint.proceed();
 		return ret;
 	}
-	@Around // adivce중 하나.
+	@Around 
 	("execution(* controller.Cart*.chkm*(..)) && args(..,session)")
-	public Object C_M_LoginCheck(ProceedingJoinPoint joinPoint,HttpSession session) throws Throwable{
-		Member loginmem = (Member)session.getAttribute("loginmem");
+	public Object CartMemberLoginCheck(ProceedingJoinPoint joinPoint) throws Throwable{
+		Member loginmem = null;
+		for(Object o : joinPoint.getArgs()) {
+			if(o instanceof HttpSession) {
+				HttpSession session = (HttpSession)o;
+				loginmem = (Member)session.getAttribute("loginmem");
+			}
+		}
 		if(loginmem == null) {
 			throw new LoginException("로그인 후 거래하세요","../member/login.dog");
 		}
@@ -36,34 +50,43 @@ public class MemberLoginAspect {
 		return ret;
 	}
 	
-//	@Around // adivce중 하나.
-//	("execution(* controller.*.chks*(..)) && args(..,session)")
-//	public Object shelterLoginCheck(ProceedingJoinPoint joinPoint,HttpSession session) throws Throwable{
-//		Member loginsmem = (Member)session.getAttribute("loginsmem");
-//		if(loginsmem == null) {
-//			throw new LoginException("보호소 관리자로 로그인 후 거래하세요","../member/login.shop");
-//		}
-//		Object ret = joinPoint.proceed();
-//		return ret;
-//	}
-//	
-//	@Around 
-//	("execution(* controller.*.chks*(..)) && args(..,session)")
-//	//chkmi : check member id 를 줄여서 표현 해보았습니다. 본인확인 부분은 chkmi 로 해주세요
-//	
-//	public Object memberCheck(ProceedingJoinPoint joinPoint,String member_id) throws Throwable{
-//		Member loginmem = null;
-//		for(Object o : joinPoint.getArgs()) {
-//			if(o instanceof HttpSession) {
-//				HttpSession session = (HttpSession)o;
-//				loginmem = (Member)session.getAttribute("loginmem");
-//			}
-//		}
-//		if(loginmem == null) {
-//			throw new LoginException("로그인 후 거래하세요","login.dog");
-//		}else if(!loginmem.getMember_id().equals("admin") && !loginmem.getMember_id().equals(member_id)){
-//			throw new LoginException("본인만 거래가능합니다.","main.dog");
-//		}
-//		return joinPoint.proceed();
-//	}
+	@Around // 보호소 관리자의 로그인 여부를 판단합니다.  	
+//	("execution(* controller.<Item>*.chks*(..)) && args(..,session)")  : <Item> 부분을 바꾸어서 사용하시길 바랍니다.
+//	매개변수의 위치는 상관이 없으나 HttpSession를 꼭 포함해야 합니다.
+	("execution(* controller.Item*.chks*(..))")
+	public Object shelterLoginCheck(ProceedingJoinPoint joinPoint) throws Throwable{
+		Member loginsmem = null;
+		for(Object o : joinPoint.getArgs()) {
+			if(o instanceof HttpSession) {
+				HttpSession session = (HttpSession)o;
+				loginsmem = (Member)session.getAttribute("loginsmem");
+			}
+		}
+		if(loginsmem == null) {
+			throw new LoginException("보호소 관리자로 로그인 후 거래하세요","../member/login.shop");
+		}
+		Object ret = joinPoint.proceed();
+		return ret;
+	}
+	
+	@Around 
+//		chkmi : check id 를 줄여서 표현 해보았습니다. 본인확인 부분입니다.
+//		("execution(* controller.<Item>*.chki*(..))") : <Item> 부분을 바꾸어서 사용하시길 바랍니다.
+//	매개변수의 위치는 상관이 없으나 HttpSession를 꼭 포함해야 합니다.
+	("execution(* controller.Item*.chki*(..))")
+	public Object memberCheck(ProceedingJoinPoint joinPoint,String member_id) throws Throwable{
+		Member loginmem = null;
+		for(Object o : joinPoint.getArgs()) {
+			if(o instanceof HttpSession) {
+				HttpSession session = (HttpSession)o;
+				loginmem = (Member)session.getAttribute("loginmem");
+			}
+		}
+		if(loginmem == null) {
+			throw new LoginException("로그인 후 거래하세요","login.dog");
+		}else if(!loginmem.getMember_id().equals("admin") && !loginmem.getMember_id().equals(member_id)){
+			throw new LoginException("본인만 거래가능합니다.","main.dog");
+		}
+		return joinPoint.proceed();
+	}
 }
