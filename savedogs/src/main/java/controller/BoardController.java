d5af45757b64819c1ed85be59d01d99578ea7a66
Hyesu-K <sarangheo12@naver.com>
2020-08-19 17:37:52 +0900

    ㅎㅅㅎ

--- savedogs/src/main/java/controller/BoardController.java
+++ savedogs/src/main/java/controller/BoardController.java
@@ -1,15 +1,91 @@
 package controller;
 
+import java.io.File;
+
+import javax.mail.Session;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import exception.BoardException;
+import logic.Board;
+import logic.DogService;
+
+
 
 @Controller
 @RequestMapping("board")
 public class BoardController {
+	@Autowired
+	private DogService service;
+	
 	@RequestMapping("*")
 	public ModelAndView noticeList() {
 		ModelAndView mav = new ModelAndView();
 		return mav;
 	}
+	
+	@GetMapping("noticeWrite") 
+	public ModelAndView writeform(Model model, HttpServletRequest request) {
+		ModelAndView mav= new ModelAndView();
+		model.addAttribute(new Board());
+		return mav;
+	}
+	
+	
+	@PostMapping("noticeWrite")
+	public ModelAndView nwrite(@Valid Board board, BindingResult bresult, HttpServletRequest request, Session session) {
+		ModelAndView mav = new ModelAndView();
+		if(bresult.hasErrors()) {
+			mav.getModel().putAll(bresult.getModel());
+			return mav;
+		}
+		try {
+			service.boardWrite(board, request);
+			if(board.getType().equals("1")) {
+				mav.setViewName("redirect:noticeList.dog");
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+			throw new BoardException("게시글 등록에 실패했습니다","write.shop");
+		}
+		return mav;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@RequestMapping("imgupload")
+	public String imgupload(MultipartFile upload, String CKEditorFuncNum, HttpServletRequest request, Model model) {
+		String path = request.getServletContext().getRealPath("/") + "board/imgfile/"; 
+		File f = new File(path);
+		if(!f.exists()) f.mkdirs();
+		if(!upload.isEmpty()) { 
+			File file = new File(path, upload.getOriginalFilename()); 
+			try {
+				upload.transferTo(file); 
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		String fileName = request.getServletContext().getContextPath()+"/board/imgfile/"+upload.getOriginalFilename();
+		model.addAttribute("fileName",fileName);
+		model.addAttribute("CKEditorFuncNum",CKEditorFuncNum);
+		return "ckedit";
+	}
+	
 }
--- savedogs/src/main/webapp/WEB-INF/view/board/noticeWrite.jsp
+++ savedogs/src/main/webapp/WEB-INF/view/board/noticeWrite.jsp
@@ -11,9 +11,23 @@
 <div class="main_div">
 <h2>공지사항 작성</h2>
 <hr>
-<div >
-	
-</div>
+<form:form modelAttribute="board" action="noticeWrite.dog" enctype="multipart/form-data" name="f">	
+	<form:hidden path="type" value="1"/>
+	<form:hidden path="member_id" value="${sessionScope.loginadmin.member_id }"/>
+	<table>
+		<tr><td>제목</td><td><form:input path="subject"/><font color="red"><form:errors path="subject"/></font></td></tr>
+		<tr><td>내용</td><td><form:textarea path="content" rows="15" cols="80"/>
+		<script>CKEDITOR.replace("content",{
+			filebrowserImageUploadUrl : "imgupload.dog"
+		});</script>
+		<font color="red"><form:errors path="content"/></font></td></tr>
+		<tr><td>첨부파일</td><td><input type="file" name="file1"/></td></tr>
+		<tr><td colspan="2" class="btn_td">
+				<input type="submit" value="등록" class="s_btn">
+			</td>
+		</tr>
+	</table>
+</form:form>
 </div>
 </body>
 </html>
